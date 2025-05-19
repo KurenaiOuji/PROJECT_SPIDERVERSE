cb91443c0f452887aab4563ede82d3d4033bce46
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2025-05-18 21:23:53 -0600

    Camera
    Character move to camera.forward

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,13 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     [Header("References")]
     private CharacterController _controller;
+    [SerializeField] private Transform _camera;
 
     [Header("Movement Settings")]
-    [SerializeField] private float _Speed = 5f;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _turningSpeed = 2f;
 
     [Header("Inputs")]
-    private float _HorizontalInput;
-    private float _VerticalInput;
+    private float _horizontalInput;
+    private float _verticalInput;
 
     private void Start()
     {
@@ -29,20 +31,34 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         GroundMovement();
+        Turn();
     }
 
     private void GroundMovement()
     {
-        Vector3 move = new Vector3 (_HorizontalInput, -9.81f, _VerticalInput);
+        Vector3 move = new Vector3 (_horizontalInput, -9.81f, _verticalInput);
+        move = transform.TransformDirection(move);
 
-        move *= _Speed;
+        move *= _speed;
 
         _controller.Move(move * Time.deltaTime);
     }
 
+    private void Turn()
+    {
+        if (Mathf.Abs(_horizontalInput) > 0 || Mathf.Abs(_verticalInput) > 0)
+        {
+            Vector3 currentLookDirection = _camera.forward;
+            currentLookDirection.y = 0;
+
+            Quaternion targetRotation = Quaternion.LookRotation(currentLookDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * _turningSpeed);
+        }
+    }
+
     private void InputManagement()
     {
-        _HorizontalInput = Input.GetAxis("Horizontal");
-        _VerticalInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
     }
 }
