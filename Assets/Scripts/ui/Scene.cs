ae86a2ddf8d81c0bd165702fe414153455dc5aaa
MaxIb62 <118229351+Greyslor@users.noreply.github.com>
2025-05-28 21:01:07 -0600

    Avance de UI

---
+++ Assets/Scripts/ui/Scene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
